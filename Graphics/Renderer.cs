2a1603a01449ac6c9f392022663b12f8fbcb9799
Kalvin Garcia <plutea.leaf@gmail.com>
2023-03-18 17:57:33 -0500

    Initializing the project.

---
+++ Graphics/Renderer.cs
@@ -0,0 +1,30 @@
+﻿using GLFW;
+
+namespace ArcadiaEngine.Graphics {
+    abstract class Renderer { // Game Renderer
+        public void run() {
+            _initialize();
+            WindowManager.initialize();
+
+            _load();
+
+            while (!Glfw.WindowShouldClose(WindowManager.window)) {
+                Timer.delta_time = (float)Glfw.Time - Timer.elapsed_time;
+                Timer.elapsed_time = (float)Glfw.Time;
+
+                _update();
+
+                Glfw.PollEvents();
+
+                _draw();
+            }
+
+        }
+
+        protected abstract void _initialize();
+        protected abstract void _load();
+        protected abstract void _update();
+        protected abstract void _draw();
+
+    }
+}
---
+++ Graphics/WindowManager.cs
@@ -0,0 +1,57 @@
+﻿using GLFW;
+using ArcadiaEngine.Graphics.OpenGL;
+using System.Numerics;
+using System.Drawing;
+
+namespace ArcadiaEngine.Graphics {
+    static class WindowManager {
+        // Game display window
+        public static Window window { get; set; }
+        public static Vector2 window_size { get; set; }
+
+        // Debug simulation window
+        public static Window debug { get; set; }
+        public static Vector2 debug_size { get; set; }
+
+        private static void _initialize_window() {
+            //window_size.Add(title, new Vector2(width, height));
+            window_size = Settings.window_size;
+
+            Glfw.Init();
+
+            // using OpenGL 3.3 core profile
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+
+            // focus the window and disable resize
+            Glfw.WindowHint(Hint.Focused, true);
+            Glfw.WindowHint(Hint.Resizable, false);
+
+            window = Glfw.CreateWindow((int)Settings.window_size.X, (int)Settings.window_size.Y, Settings.window_name, Monitor.None, Window.None);
+            if(window == Window.None) {
+                // somethign went wrong
+                return;
+            }
+
+            // center the window on the screen
+            Rectangle screen = Glfw.PrimaryMonitor.WorkArea;
+            int x = (screen.Width - (int)Settings.window_size.X) / 2;
+            int y = (screen.Height - (int)Settings.window_size.Y) / 2;
+            Glfw.SetWindowPosition(window, x, y);
+
+            Glfw.MakeContextCurrent(window);
+            GL.Import(Glfw.GetProcAddress);
+
+            GL.glViewport(0, 0, (int)Settings.window_size.X, (int)Settings.window_size.Y);
+            // vsync
+            Glfw.SwapInterval(Settings.vsync); // 0 is off, 1 is on
+        }
+
+        public static void initialize() {
+            _initialize_window();
+        }
+
+
+    }
+}
