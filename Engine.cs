4836fcbe5f31a81b5f09b25a1badee70b004ef05
Kalvin Garcia <plutea.leaf@gmail.com>
2023-03-25 00:34:56 -0500

    Separated the game engine and rendering engine to introduce physics and audio engines later

---
+++ Engine.cs
@@ -0,0 +1,36 @@
+﻿using GLFW;
+using ArcadiaEngine.Graphics;
+// using ArcadiaEngine.Physics;
+// using ArcadiaEngine.Audio;
+
+namespace ArcadiaEngine {
+    abstract class Engine {
+        public virtual void run() {
+            Settings.load();
+
+            WindowManager.initialize();
+            Renderer.initialize();
+            initialize();
+
+            load();
+
+            while (!Glfw.WindowShouldClose(WindowManager.window)) {
+                Timer.tick();
+                update();
+
+                Glfw.PollEvents();
+
+                Renderer.clear_color();
+                draw();
+                Renderer.swap_buffers();
+            }
+
+            Settings.save();
+        }
+
+        protected abstract void initialize();
+        protected abstract void load();
+        protected abstract void update();
+        protected abstract void draw();
+    }
+}
--- Graphics/Renderer.cs
+++ Graphics/Renderer.cs
@@ -1,30 +1,56 @@
-﻿using GLFW;
+﻿using System.IO;
+using System.Numerics;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+using OpenGL;
+using static OpenGL.Gl;
+using static GLFW.Glfw;
+
+using ArcadiaEngine.Graphics.Sprites;
 
 namespace ArcadiaEngine.Graphics {
-    abstract class Renderer { // Game Renderer
-        public void run() {
-            _initialize();
-            WindowManager.initialize();
+    static class Renderer { // Game Renderer
+        private static Camera camera;
+        private static Dictionary<string, SpriteInfo> sprite_table;
 
-            _load();
+        public static Vector4 background_color { get; set; }
 
-            while (!Glfw.WindowShouldClose(WindowManager.window)) {
-                Timer.delta_time = (float)Glfw.Time - Timer.elapsed_time;
-                Timer.elapsed_time = (float)Glfw.Time;
+        private static void load_sprites() {
+            /*
+            if (!File.Exists(Settings.sprite_info_file))
+                throw new FileNotFoundException("ARCADIA ENGINE ERROR: Could not find settings information file.");
+            if (Path.GetExtension(Settings.sprite_info_file) != ".json")
+                throw new FileLoadException("ARCADIA ENGINE ERROR: Could not load file because it is not of type JSON.");
+            JObject sprite_info = JObject.Parse(File.ReadAllText(Settings.sprite_info_file));
 
-                _update();
+            sprite_table = new Dictionary<string, SpriteInfo>();
 
-                Glfw.PollEvents();
+            foreach(var sprite in sprite_info)
+                sprite_table.Add(sprite.Key, sprite.Value.ToObject<SpriteInfo>());
+            */
+        }
 
-                _draw();
-            }
+        unsafe public static void initialize() {
+            Enable(EnableCap.Blend);
+            BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            camera = new Camera(WindowManager.window_size / 2.0f, 1.0f);
+
+            load_sprites();
+        }
+
+        public static void draw() {
 
         }
 
-        protected abstract void _initialize();
-        protected abstract void _load();
-        protected abstract void _update();
-        protected abstract void _draw();
+        public static void clear_color() {
+            ClearColor(background_color.X, background_color.Y, background_color.Z, background_color.W);
+            Clear(ClearBufferMask.ColorBufferBit);
+        }
 
+        public static void swap_buffers() {
+            SwapBuffers(WindowManager.window);
+        }
     }
 }
