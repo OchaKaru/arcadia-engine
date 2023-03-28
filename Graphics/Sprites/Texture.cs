2a8400dd9f1f2bb3919a592297f55679a6cf377c
Kalvin Garcia <plutea.leaf@gmail.com>
2023-03-28 16:05:57 -0500

    Corrected an error with the image loading algorithm

--- Graphics/Sprites/Texture.cs
+++ Graphics/Sprites/Texture.cs
@@ -1,7 +1,8 @@
-﻿using System.Drawing;
-using System.IO;
-using System.Runtime.Versioning;
+﻿using System.IO;
+using System.Runtime.CompilerServices;
 
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
 
@@ -12,12 +13,10 @@ namespace ArcadiaEngine.Graphics.Sprites {
         public int texture { get; }
         public Vector2 texture_size { get; }
 
-        [SupportedOSPlatform("windows")]
         public Texture(string texture_path, TextureWrapMode wrap_x_param, TextureWrapMode wrap_y_param, TextureMinFilter min_filter, TextureMagFilter mag_filter) {
             if(File.Exists(texture_path) is not true)
                 throw new FileNotFoundException("ARCADIA ENGINE ERROR: The image file specified could not be found.");
-
-            Bitmap image = new Bitmap(texture_path) ?? throw new ImageNotLoadedError("ARCADIA ENGINE ERROR: The texture image at the path specified could not be loaded.");
+            Image<Rgba32> image = Image.Load<Rgba32>(texture_path) ?? throw new ImageNotLoadedError("ARCADIA ENGINE ERROR: The texture image at the path specified could not be loaded.");
 
             texture_size = new Vector2(image.Width, image.Height);
 
@@ -34,28 +33,18 @@ namespace ArcadiaEngine.Graphics.Sprites {
             GL.BindTexture(TextureTarget.Texture2D, 0);
         }
 
-        [SupportedOSPlatform("windows")]
-        public Texture(string texture_path) : this(texture_path, TextureWrapMode.Repeat, TextureWrapMode.Repeat, TextureMinFilter.Linear, TextureMagFilter.Linear) { }
+        public Texture(string texture_path) : this(texture_path, TextureWrapMode.Repeat, TextureWrapMode.Repeat, TextureMinFilter.Nearest, TextureMagFilter.Nearest) { }
 
         public void bind(int index) {
             GL.ActiveTexture(TextureUnit.Texture0 + index);
             GL.BindTexture(TextureTarget.Texture2D, texture);
         }
 
-        [SupportedOSPlatform("windows")]
-        public static byte[] image_byte_data(Bitmap img) {
-            byte[] data = new byte[4 * (img.Width * img.Height)];
-            int offset = 0;
-            for(int i = 0; i < img.Width; i++) {
-                for(int j = img.Height - 1; j >= 0; j--) {
-                    Color pixel = img.GetPixel(i, j);
-                    data[offset + 0] = pixel.R;
-                    data[offset + 1] = pixel.G;
-                    data[offset + 2] = pixel.B;
-                    data[offset + 3] = pixel.A;
-                    offset += 4;
-                }
-            }
+        public static byte[] image_byte_data(Image<Rgba32> img) {
+            byte[] data = new byte[Unsafe.SizeOf<Rgba32>() * (img.Width * img.Height)];
+            
+            img.CopyPixelDataTo(data);
+
             return data;
         }
     }
