66d400cec6ac893ecaa66cbdbc877b1eb79758e7
Kalvin Garcia <plutea.leaf@gmail.com>
2023-04-01 19:25:40 -0500

    Added the entity abstract class that all game objects will be created from.

--- Common/Entity.cs
+++ Common/Entity.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenTK.Graphics.ES11;
+using OpenTK.Mathematics;
 
 namespace ArcadiaEngine.Common {
-    class Entity {
+    public abstract class Entity {
+        public Vector2 entity_position { get; set; }
+        public float rotation { get; set; }
+        public Vector2 entity_velocity { get; set; }
 
-        public Entity() { }
+        public bool dead { get; set; }
+
+        public Vector4 entity_color { get; set; }
+
+        public Entity(Vector2 initial_position) {
+            entity_position = initial_position;
+            rotation = 0;
+            entity_velocity = new Vector2(0, 0);
+
+            dead = false;
+
+            entity_color = new Vector4(1, 1, 1, 1);
+        }
+
+        public void move() {
+            entity_position += entity_velocity;
+        }
+
+        public void get_hit_box() {
+
+        }
     }
 }
