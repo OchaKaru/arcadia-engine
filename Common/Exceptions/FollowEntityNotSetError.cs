14a8fde8148fbe3d9a6e37e5c33b456dbb8b5c67
Kalvin Garcia <plutea.leaf@gmail.com>
2023-04-03 12:57:00 -0500

    Added a new exception for when the user hasn't set the entity for the camera to follow.

---
+++ Common/Exceptions/FollowEntityNotSetError.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArcadiaEngine.Common.Exceptions {
+    internal class FollowEntityNotSetError : Exception {
+        public FollowEntityNotSetError(string message) : base(message) { }
+        public FollowEntityNotSetError() : base() { }
+    }
+}
