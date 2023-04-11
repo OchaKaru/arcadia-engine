5e410942aa3416138018ee0b9c6d05a2cf5fdcef
Kalvin Garcia <plutea.leaf@gmail.com>
2023-04-10 20:52:32 -0500

    Made the Timer public, but made the tick function internal.

--- Physics/Timer.cs
+++ Physics/Timer.cs
@@ -1,11 +1,11 @@
 ﻿using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace ArcadiaEngine.Physics {
-    static class Timer {
+    public static class Timer {
         public static float delta_time { get; set; }
         public static float elapsed_time { get; set; }
 
-        public static void tick() {
+        internal static void tick() {
             delta_time = (float)GLFW.GetTime() - elapsed_time;
             elapsed_time = (float)GLFW.GetTime();
         }
