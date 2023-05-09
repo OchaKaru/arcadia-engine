4deea3d49d8d1369413d5935e34cd768ae144200
Kalvin Garcia <plutea.leaf@gmail.com>
2023-05-08 22:20:22 -0500

    I decided to restructure the way that shapes and physics are going to work because this was giving me a lot of issues.

--- Common/Exceptions/ShapeNotValidError.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ArcadiaEngine.Common.Exceptions {
-    internal class ShapeNotValidError : Exception {
-        public ShapeNotValidError(string message) : base(message) { }
-        public ShapeNotValidError() : base() { }
-    }
-}
